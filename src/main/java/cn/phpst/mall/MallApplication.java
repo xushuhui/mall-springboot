934a7d8e511ef30cb1271a59bcd5d1930c98473b
xushuhui <474497097@qq.com>
2019-12-12 11:54:36 +0800

    refactor code

---
+++ src/main/java/cn/phpst/mall/MallApplication.java
@@ -0,0 +1,16 @@
+package cn.phpst.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xushuhui
+ */
+@SpringBootApplication
+public class MallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallApplication.class, args);
+	}
+
+}
