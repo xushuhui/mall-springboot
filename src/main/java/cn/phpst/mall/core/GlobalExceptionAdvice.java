8ca70125b7be009a8a1c1702d5457f81e6bd4bfb
xushuhui <474497097@qq.com>
2020-03-12 16:39:58 +0800

    global exception

---
+++ src/main/java/cn/phpst/mall/core/GlobalExceptionAdvice.java
@@ -0,0 +1,7 @@
+package cn.phpst.mall.core;
+class GlobalExceptionAdvice {
+    public void handleException(){
+        System.out.println("GlobalExceptionAdvice.handleException");
+    }
+} git config --local user.email "474497097@qq.com"
+  git config --local user.name "xushuhui"
