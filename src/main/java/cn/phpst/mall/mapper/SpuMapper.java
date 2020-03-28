b5352ec50f07d421b5b2305a2166d5d223ca1963
xushuhui <474497097@qq.com>
2020-03-28 19:45:54 +0800

    add spu

---
+++ src/main/java/cn/phpst/mall/mapper/SpuMapper.java
@@ -0,0 +1,9 @@
+package cn.phpst.mall.mapper;
+
+import cn.phpst.mall.model.SpuDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpuMapper extends BaseMapper<SpuDO> {
+}
